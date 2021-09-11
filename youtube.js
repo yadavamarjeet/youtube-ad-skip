e0406a3135d4da9136a378a12beb27a81b114b5a
Amarjeet Yadav <42440865+yadavamarjeet@users.noreply.github.com>
2021-09-11 23:27:08 +0530

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,13 +1,21 @@
 const click = (class_n) => {
-    const buttons = document.getElementsByClassName(class_n);
-    for (const button of buttons) {
-      button.click();
-      console.log("Skipped Ad Successfully.");
-    }
+  const buttons = document.getElementsByClassName(class_n);
+  for (const button of buttons) {
+    button.click();
+    console.log("Skipped Ad Successfully.");
   }
-  
-  setInterval(() => {
-    click("ytp-ad-skip-button-text");
-    click("ytp-ad-overlay-close-button");
-  }, 100);
-  console.log("Skipped Ad Successfully.");
+};
+
+const delete_element = (class_n) => {
+  const elements = document.getElementsByClassName(class_n);
+  for (const element of elements) {
+    element.remove();
+    console.log("Successfully deleted a element.");
+  }
+};
+
+setInterval(() => {
+  click("ytp-ad-skip-button-text");
+  click("ytp-ad-overlay-close-button");
+  delete_element("ff-sans");
+}, 100);
